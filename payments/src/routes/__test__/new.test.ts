9b43a6020fb3b1810519526a45b258dee5c2c4d8
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2020-09-12 16:00:43 -0500

    testing scripts

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -1,25 +1,23 @@
-import request from 'supertest'
 import mongoose from 'mongoose'
+import request from 'supertest'
+import { OrderStatus } from '@dttickets/common'
 import { app } from '../../app'
 import { Order } from '../../models/order'
-import { OrderStatus } from '@dttickets/common'
 import { stripe } from '../../stripe'
 import { Payment } from '../../models/payment'
-
-// jest.mock('../../stripe')
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
   await request(app)
     .post('/api/payments')
     .set('Cookie', global.signin())
     .send({
-      token: 'asdf',
+      token: 'asldkfj',
       orderId: mongoose.Types.ObjectId().toHexString(),
     })
     .expect(404)
 })
 
-it('returns a 401 when puchasing an order that does not belong to the user', async () => {
+it('returns a 401 when purchasing an order that doesnt belong to the user', async () => {
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId: mongoose.Types.ObjectId().toHexString(),
@@ -27,22 +25,20 @@ it('returns a 401 when puchasing an order that does not belong to the user', asy
     price: 20,
     status: OrderStatus.Created,
   })
-
   await order.save()
 
   await request(app)
     .post('/api/payments')
     .set('Cookie', global.signin())
     .send({
-      token: 'asdf',
+      token: 'asldkfj',
       orderId: order.id,
     })
     .expect(401)
 })
 
-it('returns a 400 when purchasing an cancelled order', async () => {
+it('returns a 400 when purchasing a cancelled order', async () => {
   const userId = mongoose.Types.ObjectId().toHexString()
-
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId,
@@ -50,7 +46,6 @@ it('returns a 400 when purchasing an cancelled order', async () => {
     price: 20,
     status: OrderStatus.Cancelled,
   })
-
   await order.save()
 
   await request(app)
@@ -58,7 +53,7 @@ it('returns a 400 when purchasing an cancelled order', async () => {
     .set('Cookie', global.signin(userId))
     .send({
       orderId: order.id,
-      token: 'asdf',
+      token: 'asdlkfj',
     })
     .expect(400)
 })
@@ -66,7 +61,6 @@ it('returns a 400 when purchasing an cancelled order', async () => {
 it('returns a 201 with valid inputs', async () => {
   const userId = mongoose.Types.ObjectId().toHexString()
   const price = Math.floor(Math.random() * 100000)
-
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
     userId,
@@ -74,7 +68,6 @@ it('returns a 201 with valid inputs', async () => {
     price,
     status: OrderStatus.Created,
   })
-
   await order.save()
 
   await request(app)
@@ -98,6 +91,5 @@ it('returns a 201 with valid inputs', async () => {
     orderId: order.id,
     stripeId: stripeCharge!.id,
   })
-
   expect(payment).not.toBeNull()
 })
--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -1,7 +1,5 @@
 import { MongoMemoryServer } from 'mongodb-memory-server'
 import mongoose from 'mongoose'
-// import request from 'supertest'
-// import { app } from '../app'
 import jwt from 'jsonwebtoken'
 
 declare global {
@@ -14,15 +12,14 @@ declare global {
 
 jest.setTimeout(30000)
 
-let mongo: any
-
 jest.mock('../nats-wrapper')
 
-process.env.STRIPE_KEY =
-  'sk_test_51HQEJMBQszSNQ9z1QyQ1p0BZcsLc2MdhZFQwG2OmyaU8NIwzVJt0jK4uB6THRaFkD1fpKUDagFQm3qOzJ4hSK785000WIEjHEW'
+process.env.STRIPE_KEY = 'sk_test_c7MVBsHhjJ1dgOTfNQkoeabk00G1i7V3wz'
 
+let mongo: any
 beforeAll(async () => {
   process.env.JWT_KEY = 'asdfasdf'
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
   mongo = new MongoMemoryServer()
   const mongoUri = await mongo.getUri()
@@ -35,7 +32,6 @@ beforeAll(async () => {
 
 beforeEach(async () => {
   jest.clearAllMocks()
-
   const collections = await mongoose.connection.db.collections()
 
   for (let collection of collections) {
@@ -49,16 +45,16 @@ afterAll(async () => {
 })
 
 global.signin = (id?: string) => {
-  // Build a JWT payload. { id, email }
+  // Build a JWT payload.  { id, email }
   const payload = {
     id: id || new mongoose.Types.ObjectId().toHexString(),
     email: 'test@test.com',
   }
 
-  // Create the JWT
+  // Create the JWT!
   const token = jwt.sign(payload, process.env.JWT_KEY!)
 
-  // Build session Object
+  // Build session Object. { jwt: MY_JWT }
   const session = { jwt: token }
 
   // Turn that session into JSON
