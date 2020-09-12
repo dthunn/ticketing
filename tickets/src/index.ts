bb9b1bf0ae24a1a3035c54ed8d0ec3c52371f8c3
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2020-09-12 15:49:09 -0500

    testing is so fun

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,7 +6,7 @@ import { OrderCancelledListener } from './events/listeners/order-cancelled-liste
 
 // Connect to Mongodb and start app
 const start = async () => {
-  console.log('starting')
+  console.log('starting...')
 
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
